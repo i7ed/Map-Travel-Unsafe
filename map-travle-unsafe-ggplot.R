b5e86b8efd85870a5d153489517748306ea47e62
Thomas Gredig <thomas.gredig@csulb.edu>
2019-05-13 15:27:52 -0700

    update mapstools portion

--- map-travle-unsafe-ggplot.R
+++ map-travle-unsafe-ggplot.R
@@ -8,16 +8,16 @@ data(wrld_simpl)
 # make a plot of the entire world
 plot(wrld_simpl)
 
-
-sw <- wrld_simpl[wrld_simpl$NAME=="France",]
+wrld_simpl$NAME
+sw <- wrld_simpl[wrld_simpl$NAME=="Viet Nam",]
 plot(sw)
 
 
 wrld_simpl@data$id <- wrld_simpl@data$NAME
-wrld <- fortify(wrld_simpl, region="id")
-wrld <- subset(wrld, id != "Antarctica") # we don't rly need Antarctica
+wrld <- fortify(wrld_simpl$data, region="NAME")
+wrld <- subset(wrld, NAME != "Antarctica") # we don't rly need Antarctica
 
 
 ggplot() + 
-    geom_map(data=sw, map=sw, aes(map_id=id, x=long, y=lat), fill="white", color="#7f7f7f", size=0.25)
+    geom_map(data=wrld, map=wrld, aes(map_id=id, x=long, y=lat), fill="white", color="#7f7f7f", size=0.25)
 
